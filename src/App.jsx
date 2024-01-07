306120c4e973cce8bb524fcdc474f332a8ef14c5
Devang Vishnu <thedevangvishnu@gmail.com>
2024-01-07 12:08:13 +0530

    Add clarifai api and add image into DetectSection

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,81 @@ import Navigation from "./components/navigation/Navigation";
 import HeroSection from "./components/hero-section/HeroSection";
 import DetectSection from "./components/detect-section/DetectSection";
 
+// setup clarifai and return request options using it
+const returnRequestUsingClarafai = (imageUrl) => {
+  // Your PAT (Personal Access Token) can be found in the portal under Authentification
+  const PAT = "0b67fd927a9441bea48d88e5560ec538";
+  // Specify the correct user_id/app_id pairings
+  // Since you're making inferences outside your app's scope
+  const USER_ID = "dv080499";
+  const APP_ID = "smart-brain";
+
+  const IMAGE_URL = imageUrl;
+
+  const raw = JSON.stringify({
+    user_app_id: {
+      user_id: USER_ID,
+      app_id: APP_ID,
+    },
+    inputs: [
+      {
+        data: {
+          image: {
+            url: IMAGE_URL,
+          },
+        },
+      },
+    ],
+  });
+
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      Authorization: "Key " + PAT,
+    },
+    body: raw,
+  };
+
+  return requestOptions;
+};
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+      imageUrl: "",
+    };
+  }
+
+  onInputChange = (event) => {
+    this.setState({ input: event.target.value });
+  };
+
+  onButtonPress = (event) => {
+    this.setState({ imageUrl: this.state.input });
+
+    fetch(
+      "https://api.clarifai.com/v2/models/" + "face - detection" + "/outputs",
+      returnRequestUsingClarafai
+    )
+      .then((response) => response.json())
+      .then((result) => console.log(result))
+      .catch((error) => console.log("error", error));
+  };
+
   render() {
     return (
       <div className="w-full h-screen bg-slate-200">
         <Navigation />
         <div className="flex flex-col items-center gap-10">
           <HeroSection />
-          <DetectSection />
+          <DetectSection
+            onInputChange={this.onInputChange}
+            onButtonPress={this.onButtonPress}
+            imageUrl={this.state.imageUrl}
+          />
         </div>
       </div>
     );
--- src/components/detect-section/DetectSection.jsx
+++ src/components/detect-section/DetectSection.jsx
@@ -1,12 +1,25 @@
-const DetectSection = () => {
+const InputSection = ({ onInputChange, onButtonPress, imageUrl }) => {
   return (
     <div className="flex shadow-lg">
-      <input className="px-4" type="text" placeholder="Enter the image URL" />
-      <button className="bg-slate-900 text-white px-8 py-2 tracking-widest">
+      <input
+        className="px-4"
+        type="text"
+        placeholder="Enter the image URL"
+        onChange={onInputChange}
+      />
+      <button
+        className="bg-slate-900 text-white px-8 py-2 tracking-widest"
+        onClick={onButtonPress}
+      >
         DETECT
       </button>
+
+      {/* image section */}
+      <div className="w-96 h-auto">
+        <img src={imageUrl} alt="Photo entered using the URL" />
+      </div>
     </div>
   );
 };
 
-export default DetectSection;
+export default InputSection;
