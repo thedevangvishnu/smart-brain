b2367738f201d0644fe3372b69e13353f3c4db9f
Devang Vishnu <thedevangvishnu@gmail.com>
2024-01-07 11:11:37 +0530

    Convert the App component to class component
    To follow along the course, convert the App component
    which is a fucntional component to a class component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
+import { Component } from "react";
 import Navigation from "./components/navigation/Navigation";
 import HeroSection from "./components/hero-section/HeroSection";
 import DetectSection from "./components/detect-section/DetectSection";
 
-const App = () => {
-  return (
-    <div className="w-full h-screen bg-slate-200">
-      <Navigation />
-      <div className="flex flex-col items-center gap-10">
-        <HeroSection />
-        <DetectSection />
+class App extends Component {
+  render() {
+    return (
+      <div className="w-full h-screen bg-slate-200">
+        <Navigation />
+        <div className="flex flex-col items-center gap-10">
+          <HeroSection />
+          <DetectSection />
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default App;
