fab9f9dfffd4fa0d385be5838c824fffff71e529
Devang Vishnu <thedevangvishnu@gmail.com>
2024-01-13 11:20:17 +0530

    Convert SignIn into a smart-component
    SignIn needs to have a state so that the email and password provided
    by the user could be stored. Create a functionality that takes the
    data from the state and communicates the "/singin" route in order
    to validate whether the email and the password provided are valid
    or not.

--- smart-brain-frontend/src/components/sign-in/SignIn.jsx
+++ smart-brain-frontend/src/components/sign-in/SignIn.jsx
@@ -1,57 +1,97 @@
-const SignIn = ({ onSignIn }) => {
-  return (
-    <div className="w-full max-w-xs">
-      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-        <div className="mb-4">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="email"
-          >
-            Email
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="email"
-            type="email"
-            placeholder="Enter your email"
-          />
-        </div>
-        <div className="mb-6">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="password"
-          >
-            Password
-          </label>
-          <input
-            className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-            id="password"
-            type="password"
-            placeholder="******************"
-          />
-          <p className="text-red-500 text-xs italic">
-            Please choose a password.
-          </p>
-        </div>
-        <div className="flex items-center justify-between">
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            type="button"
-            onClick={() => onSignIn("home")}
-          >
-            Sign In
-          </button>
-          <p
-            className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800 cursor-pointer"
-            href="#"
-            onClick={() => onSignIn("register")}
-          >
-            Register
-          </p>
-        </div>
-      </form>
-    </div>
-  );
-};
+import { Component } from "react";
+
+class SignIn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      singInEmail: "",
+      signInPassword: "",
+    };
+  }
+
+  onEmailChange = (event) => {
+    this.setState({ signInEmail: event.target.value });
+  };
+
+  onPasswordChange = (event) => {
+    this.setState({ signInPassword: event.target.value });
+  };
+
+  onSubmitSignIn = () => {
+    fetch("http://localhost:3000/signin", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email: this.state.signInEmail,
+        password: this.state.signInPassword,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.msg === "Sign in success") {
+          this.props.onSignIn("home");
+        }
+      });
+  };
+
+  render() {
+    const { onSignIn } = this.props;
+    return (
+      <div className="w-full max-w-xs">
+        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="email"
+            >
+              Email
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="email"
+              type="email"
+              placeholder="Enter your email"
+              onChange={this.onEmailChange}
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <input
+              className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              type="password"
+              placeholder="******************"
+              onChange={this.onPasswordChange}
+            />
+            <p className="text-red-500 text-xs italic">
+              Please choose a password.
+            </p>
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="button"
+              onClick={this.onSubmitSignIn}
+            >
+              Sign In
+            </button>
+            <p
+              className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800 cursor-pointer"
+              href="#"
+              onClick={() => onSignIn("register")}
+            >
+              Register
+            </p>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default SignIn;
